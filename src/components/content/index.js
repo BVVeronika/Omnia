1998a8829b89afd7b54ece0a478cd54ab834ec73
DESKTOP-GH7A84G\Veronichka <baranova.veronika91@gmail.com>
2017-08-01 18:19:40 +0700

    first commit

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,55 +1,20 @@
-$('.collapsible-header').click(function(){
-    var self = this;
-    var $parent = $(this).parent(); 
-    var $slick = $parent.find('.autoplay');
+var clock;
+        
+$(document).ready(function() {
+            
+    var clock;
 
-    if($slick.hasClass('slick-initialized')) {
-        $slick.slick("unslick");
-    }
-
-    setTimeout(function() {
-        $slick.slick({
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            arrows: true,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            responsive:[
-                {
-                  breakpoint: 1024,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true,
-                  }
-                },
-                {
-                  breakpoint: 425,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                  }
+            clock = $('.block-counter-clock').FlipClock({
+                clockFace: 'MinuteCounter',
+                autoStart: false,
+                callbacks: {
+                    stop: function() {
+                        $('.message').html('The clock has stopped!')
+                    }
                 }
-            ]
-        });
-
-        $parent.find(".desc-card").dotdotdot({
-            height: 50
-        });
-    })
-    
-});
-
-$(function() {
-    setTimeout(function() {
-        $('.collapsible-header').first().click();
-    });
-});
-
-$(function() {
-    $('.open-list').click(function(){
-        $('.slider-leaders').css('height', 'auto');
-        $('.open-list').css('color', '#ccc');
-    });
+            });
+                    
+            clock.setTime(194);
+            clock.setCountdown(true);
+            clock.start();
 });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import './index.scss';
 
-import './components/header';
+import 'jquery';
+import 'flipclock/compiled/flipclock';
+import 'flipclock/src/flipclock/css/flipclock.css';
+
 import './components/content';
-
-import 'materialize-css/dist/css/materialize.css';
-import 'materialize-css/dist/js/materialize.js';
-
-import 'slick-carousel/slick/slick.scss';
-import 'slick-carousel/slick/slick-theme.scss';
-import 'slick-carousel/slick/slick.js';
-
-import 'dotdotdot';
 
 const name = 'webpack2-pug-scss-boilerplate';
